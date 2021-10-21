c65f3a12b794b93d94ce6361ba9efdcb1d64877d
xTaichou <mmlowe19@gmail.com>
2021-10-21 11:14:30 +0200

    Just added the outline for the next project that I want to work on. At the moment the document just contains docstings.

---
+++ file-explorer.py
@@ -0,0 +1,10 @@
+"""
+This is a significant project as it will test your knowledge on the various concepts of Python. You need to build an app
+ that anyone uses to explore the files in their system. You can also add features like searching and copy-paste.
+ Tkinter is a commendable choice for this project as it makes the development of GUI applications fast and easy.
+
+To create the Python File Explorer using Tkinter, you have to import the filedialog module from Tkinter. This module is
+designed for opening files and directories and saving them.
+
+Idea from: https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/#20_Random_Password_Generator
+"""
